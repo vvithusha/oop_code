ac0c58690307e58ddbe4dfd71b9555931c16a05f
vvithusha <thasvithusha@gmail.com>
2026-08-09 07:33:42 +0530

    dimanshap i print

---
+++ 09/07/2026/task01.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //first half
+    int n=5;
+
+    for(int i=0 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=n-i ; j++)
+        {
+            cout<<" ";
+        }
+
+        for(int k=1 ; k <= 2*i-1 ; k++)
+        {
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
+    int i = 5;
+    for(int i=n-1 ; i>=1 ; i--)
+    {
+        for(int j=1 ; j<=n-i ; j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1 ; k<=2*i-1 ; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
