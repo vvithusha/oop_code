4cf570a930ac39ddfaf8c96ef581dc82112876cc
vvithusha <thasvithusha@gmail.com>
2026-09-13 13:47:33 +0530

    some changes

---
+++ 13/09/2026/task01.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class student 
+{
+    private:
+      int data;
+    public:
+      student() : data(0)
+      {
+
+      }
+      ~student()
+      {
+         
+      }
+};
+int main()
+{
+    return 0;
+}
